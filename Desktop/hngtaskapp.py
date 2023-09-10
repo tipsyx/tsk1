0ebc8a01556c2625d2060084a0263013de6186b7
Your Name <you@example.com>
2023-09-10 02:52:34 +0100

    Add file link

---
+++ Desktop/hngtaskapp.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import datetime
+import pytz
+
+app = Flask(__name__)
+
+@app.route('/info', methods=['GET'])
+def get_info():
+    slack_name = request.args.get('slack_name', 'Tipsy0')
+    track = request.args.get('track', 'backend')
+    current_day = datetime.datetime.now().strftime('%A')
+    utc_time = datetime.datetime.now(pytz.UTC)
+    utc_time_str = utc_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+    github_repo_url = "https://github.com/tipsyx/tsk1.git"
+    github_file_url = "https://github.com/tipsyx/tsk1/blob/main/hngtaskapp.py"
+    
+    # Status Code of Success (HTTP 200)
+    status_code = 200
+
+    #JSON response
+    response_data = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time_str,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": status_code
+    }
+
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
